aa28df7bc562faab64a31b1164f927bf4b855f00
Clare Zhou <clare.cz@gmail.com>
2025-04-18 15:04:11 +0800

    Refactor project files for clarity and consistency, including updates to project descriptions, paths, and assembly information. Enhanced debug settings generation and improved comments for better understanding.

--- TestRunnerACAD/Properties/AssemblyInfo.cs
+++ TestRunnerACAD/Properties/AssemblyInfo.cs
@@ -1,35 +1,10 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
+using System.Reflection;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
+// Project-specific information
 [assembly: AssemblyTitle("TestRunnerACAD")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("AutoCAD Unit Test Runner")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Scott Whitney")]
-[assembly: AssemblyProduct("TestRunnerACAD")]
-[assembly: AssemblyCopyright("Copyright © Scott Whitney 2021")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("E63BEBF1-521B-4665-ADAE-C55E1C4898CE")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+// Common assembly information, including version, is in CommonAssemblyInfo.cs
+// Guid for type library
+[assembly: System.Runtime.InteropServices.Guid("E63BEBF1-521B-4665-ADAE-C55E1C4898CE")] 
